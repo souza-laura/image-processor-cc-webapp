138b7f4c1946661b193948473080d2c62bc62ccb
souza-laura <softwaredev.ment@gmail.com>
2025-02-10 23:12:56 +0100

    dummy c

--- src/main/java/it/ldas/imageprocessor/dto/model/ImageStatistics.java
+++ src/main/java/it/ldas/imageprocessor/dto/model/ImageStatistics.java
@@ -2,7 +2,6 @@ package it.ldas.imageprocessor.dto.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
